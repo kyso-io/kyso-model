6db09aa040b84ba269855bf7b8d496bdfc773a00
Eoin Murray <eoin@kyso.io>
2022-01-21 17:48:15 +0100

    trying to refix build hateos

--- src/dtos/normalized-response.dto.ts
+++ src/dtos/normalized-response.dto.ts
@@ -1,19 +1,48 @@
+import { BaseModel } from '../models/base.model'
+import { Relations } from '../models/relations.model'
+
 export class NormalizedResponseDTO<T> {
-  data: T;
+    data: T
 
-  relations: any;
+    relations: Relations | null
 
-  constructor(data: any, relations?: any) {
-    this.data = data;
+    constructor(data: BaseModel | BaseModel[] | any, relations?: Relations | undefined) {
+        this.data = data
+        if (Array.isArray(data)) {
+            if (data[0]) {
+                // We assume that all the objects in the array are of the same type...
+                if (data[0] instanceof BaseModel) {
+                    data.map((x) => x.buildHatoes(relations))
+                }
+            }
+        } else if (data instanceof BaseModel) {
+            data.buildHatoes(relations)
+        }
 
-    if (data?.buildHatoes) {
-      data.buildHatoes(relations);
+        if (!relations) {
+          this.relations = null
+          return  
+        }
+
+        this.relations = relations!
+
+        const keys: string[] = Object.keys(relations)
+        this.relations = keys.reduce((prev: Relations, key) => {
+            if (!relations[key]) return prev
+            const collection: any = relations[key]
+            const ids = Object.keys(collection!)
+
+            prev[key] = ids.reduce((last, id: string) => {
+                const model = collection[id]
+                if (model instanceof BaseModel) {
+                    model.buildHatoes(relations)
+                }
+
+                last[id] = model
+                return last
+            }, {})
+
+            return prev
+        }, {} as Relations)
     }
-
-    if (relations) {
-      this.relations = relations;
-    } else {
-      this.relations = null;
-    }
-  }
 }
--- src/models/relations.model.ts
+++ src/models/relations.model.ts
@@ -1,1 +1,9 @@
-export class Relations {}
+import { User } from './user.model'
+import { Report } from './report.model'
+import { Comment } from './comment.model'
+import { Team } from './team.model'
+import { Organization } from './organization.model'
+
+export interface Relations {
+    [key: string]: User | Report | Comment | Team | Organization | null
+}
