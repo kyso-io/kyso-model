383b554e0e499dee8dc35f93eb208ae5ebf85ee1
Daniel Andújar <daniandujar@gmail.com>
2022-01-28 10:49:09 +0100

    feat: added organization and team to kyso-config-file.model.ts

--- src/models/kyso-config-file.model.ts
+++ src/models/kyso-config-file.model.ts
@@ -1,10 +1,26 @@
+import { IsString } from 'class-validator';
+
 export class KysoConfigFile {
+  @IsString()
   public main: string;
+
+  @IsString()
   public title: string;
+
+  @IsString()
   public description: string;
-  constructor(main: string, title: string, description: string) {
+
+  @IsString()
+  public organization: string;
+
+  @IsString()
+  public team: string;
+
+  constructor(main: string, title: string, description: string, organization: string, team: string) {
     this.main = main;
     this.title = title;
     this.description = description;
+    this.organization = organization;
+    this.team = team;
   }
 }
