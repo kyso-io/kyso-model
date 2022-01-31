795241ef306db5be237fb29db4837106479e5449
Daniel Andújar <daniandujar@gmail.com>
2022-01-31 14:52:06 +0100

    fix: change tag_ids for tags and remove comment_ids for comments in report.dto.ts

--- src/dtos/report.dto.ts
+++ src/dtos/report.dto.ts
@@ -1,4 +1,5 @@
 import { BaseModel } from '../models/base.model';
+import { Comment } from '../models/comment.model';
 import { Hateoas } from '../models/hateoas.model';
 
 export class ReportDTO extends BaseModel {
@@ -11,10 +12,10 @@ export class ReportDTO extends BaseModel {
   public stars: number;
   public mark_as_star_by_user: boolean;
   public number_of_comments: number;
-  public tag_ids: string[];
+  public tags: string[];
   public description: string;
   public user_id: string;
-  public comment_ids: string[];
+  public comments: Comment[];
   public team_id: string;
   public title: string;
   public author_ids: string[];
@@ -33,10 +34,10 @@ export class ReportDTO extends BaseModel {
     stars: number,
     mark_as_star_by_user: boolean,
     number_of_comments: number,
-    tag_ids: string[],
+    tags: string[],
     description: string,
     user_id: string,
-    comment_ids: string[],
+    comments: Comment[],
     team_id: string,
     title: string,
     author_ids: string[]
@@ -51,10 +52,10 @@ export class ReportDTO extends BaseModel {
     this.stars = stars;
     this.mark_as_star_by_user = mark_as_star_by_user;
     this.number_of_comments = number_of_comments;
-    this.tag_ids = tag_ids;
+    this.tags = tags;
     this.description = description;
     this.user_id = user_id;
-    this.comment_ids = comment_ids;
+    this.comments = comments;
     this.team_id = team_id;
     this.title = title;
     this.author_ids = author_ids;
