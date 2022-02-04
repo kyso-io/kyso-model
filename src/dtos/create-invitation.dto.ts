e4e1c1f355a2c05c5a22130bb8b082a3432237cd
Daniel Andújar <daniandujar@gmail.com>
2022-02-04 12:51:07 +0100

    feat: create-invitation.dto

--- src/dtos/create-invitation.dto.ts
+++ src/dtos/create-invitation.dto.ts
@@ -1,24 +1,22 @@
 import { IsEmail, IsEnum, IsMongoId, IsString } from 'class-validator';
 import { InvitationType } from '../enums/invitation-type.enum';
-import { BaseModel } from './base.model';
 
-export class Invitation extends BaseModel {
+export class CreateInvitationDto {
   @IsEmail()
   public email: string;
-  
+
   @IsString()
   public token: string;
 
   @IsEnum(InvitationType)
   public entity: InvitationType;
-  
+
   @IsMongoId()
   public entity_id: string;
-  
+
   public payload: any;
 
   constructor(email: string, token: string, entity: InvitationType, entity_id: string, payload: any) {
-    super();
     this.email = email;
     this.token = token;
     this.entity = entity;
