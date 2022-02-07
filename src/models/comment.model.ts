076e9c1f1a552b8dca6e7b57aa3cc384537c7e66
Eoin Murray <eoin@kyso.io>
2022-02-07 19:47:50 +0100

    made comment accept discussion too

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -7,6 +7,7 @@ export class Comment extends BaseModel {
 
   public user_id: string;
 
+  @IsOptional()
   @IsMongoId()
   public report_id: string;
 
@@ -28,11 +29,12 @@ export class Comment extends BaseModel {
   @IsMongoId()
   public discussion_id?: string;
 
-  constructor(text: string, user_id: string, report_id: string, comment_id: string) {
+  constructor(text: string, user_id: string, report_id: string, discussion_id: string, comment_id: string) {
     super();
     this.text = text;
     this.user_id = user_id;
     this.report_id = report_id;
+    this.discussion_id = discussion_id;
     this.comment_id = comment_id;
     this.marked = false;
     this.edited = false;
