31b7bf035a6ec6693c45148054988342df098b20
Daniel Andújar <daniandujar@gmail.com>
2022-02-10 11:17:28 +0100

    feat: added permission KYSO_IO_REPORT_GLOBAL_PIN

--- src/enums/report-permissions.enum.ts
+++ src/enums/report-permissions.enum.ts
@@ -4,6 +4,7 @@ export enum ReportPermissionsEnum {
   EDIT = 'KYSO_IO_EDIT_REPORT',
   DELETE = 'KYSO_IO_DELETE_REPORT',
   ADMIN = 'KYSO_IO_ADMIN_REPORT',
+  GLOBAL_PIN = 'KYSO_IO_REPORT_GLOBAL_PIN',
 }
 
 export const reportPermissionsEnumList: ReportPermissionsEnum[] = [
@@ -12,4 +13,5 @@ export const reportPermissionsEnumList: ReportPermissionsEnum[] = [
   ReportPermissionsEnum.EDIT,
   ReportPermissionsEnum.DELETE,
   ReportPermissionsEnum.ADMIN,
+  ReportPermissionsEnum.GLOBAL_PIN,
 ];
