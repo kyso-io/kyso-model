1210b466c802f779688ecad811f7e8a7c2306b1f
Daniel Andújar <daniandujar@gmail.com>
2022-02-14 13:50:35 +0100

    feat: added payload property to Login

--- src/models/login.model.ts
+++ src/models/login.model.ts
@@ -1,19 +1,23 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 import { LoginProviderEnum } from '../enums/login-provider.enum';
 
 export class Login {
-  @IsOptional()
+  @IsNotEmpty()
   public username?: string;
 
+  @IsOptional()
   @IsNotEmpty()
   public password: string;
 
-  @IsNotEmpty()
+  @IsEnum(LoginProviderEnum)
   public provider: LoginProviderEnum;
 
-  constructor(password: string, provider: LoginProviderEnum, username?: string) {
+  public payload: any;
+
+  constructor(password: string, provider: LoginProviderEnum, username: string, payload: any) {
     this.password = password;
     this.provider = provider;
     this.username = username;
+    this.payload = payload;
   }
 }
