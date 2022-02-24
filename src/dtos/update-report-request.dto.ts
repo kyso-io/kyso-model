9e70d3f597b99508fc3a50ad09fc25bb029e2c38
Daniel Andújar <daniandujar@gmail.com>
2022-02-24 12:14:07 +0100

    feat: update-report-request add show_code, show_output

--- src/dtos/update-report-request.dto.ts
+++ src/dtos/update-report-request.dto.ts
@@ -1,15 +1,26 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateReportRequestDTO {
-  @IsString()
+  @IsOptional()
   @IsNotEmpty()
   public title: string;
 
+  @IsOptional()
   @IsString()
   public description: string;
 
-  constructor(title: string, description: string) {
+  @IsOptional()
+  @IsBoolean()
+  public show_code: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  public show_output: boolean;
+
+  constructor(title: string, description: string, show_code: boolean, show_output: boolean) {
     this.title = title;
     this.description = description;
+    this.show_code = show_code;
+    this.show_output = show_output;
   }
 }
