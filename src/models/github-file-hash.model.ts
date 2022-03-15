13ff9fb091b391c113686187e6e31f0a10046bd1
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-03-15 11:36:10 +0100

    feat: add path_scs in GithubFileHash

--- src/models/github-file-hash.model.ts
+++ src/models/github-file-hash.model.ts
@@ -3,10 +3,12 @@ export class GithubFileHash {
   public path: string;
   public hash: string;
   public htmlUrl: string;
-  constructor(type: string, path: string, hash: string, htmlUrl: string) {
+  public path_scs: string;
+  constructor(type: string, path: string, hash: string, htmlUrl: string, path_scs: string) {
     this.type = type;
     this.path = path;
     this.hash = hash;
     this.htmlUrl = htmlUrl;
+    this.path_scs = path_scs;
   }
 }
