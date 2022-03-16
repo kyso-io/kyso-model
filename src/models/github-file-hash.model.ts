87a85098ee634df6226a3ed5315817932265e583
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-03-16 11:31:04 +0100

    feat: add version to github-file-hash

--- src/models/github-file-hash.model.ts
+++ src/models/github-file-hash.model.ts
@@ -4,11 +4,13 @@ export class GithubFileHash {
   public hash: string;
   public htmlUrl: string;
   public path_scs: string;
-  constructor(type: string, path: string, hash: string, htmlUrl: string, path_scs: string) {
+  public version: number;
+  constructor(type: string, path: string, hash: string, htmlUrl: string, path_scs: string, version: number) {
     this.type = type;
     this.path = path;
     this.hash = hash;
     this.htmlUrl = htmlUrl;
     this.path_scs = path_scs;
+    this.version = version;
   }
 }
