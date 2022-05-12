96feef9bd7a8001d8d5333b18f5c02795ad09040
Fran Barrena <francisco@kyso.io>
2022-05-12 13:20:51 +0200

    added role_names optional property

--- src/models/resource-permissions.model.ts
+++ src/models/resource-permissions.model.ts
@@ -1,4 +1,4 @@
-import { KysoPermissions, kysoPermissionsList } from '../enums/general-permissions.enum';
+import { KysoPermissions } from '../enums/general-permissions.enum';
 
 export class ResourcePermissions {
   public name: string;
@@ -13,12 +13,15 @@ export class ResourcePermissions {
 
   public organization_id?: string;
 
-  constructor(name: string, display_name: string, permissions: KysoPermissions[], id: string, organization_inherited: boolean, organization_id: string) {
+  public role_names?: string[]
+
+  constructor(name: string, display_name: string, permissions: KysoPermissions[], id: string, organization_inherited: boolean, organization_id: string, role_names: string[]) {
     this.name = name;
     this.display_name = display_name;
     this.permissions = permissions;
     this.id = id;
     this.organization_inherited = organization_inherited;
     this.organization_id = organization_id;
+    this.role_names = role_names
   }
 }
