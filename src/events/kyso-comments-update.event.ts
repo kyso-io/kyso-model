4c3a1d10a5ea548d54ddfaef87e1190acf7435ec
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-06-20 09:21:29 +0200

    feat: kyso events

---
+++ src/events/kyso-comments-update.event.ts
@@ -0,0 +1,15 @@
+import { Comment } from '../models/comment.model';
+import { Discussion } from '../models/discussion.model';
+import { Organization } from '../models/organization.model';
+import { Report } from '../models/report.model';
+import { Team } from '../models/team.model';
+import { User } from '../models/user.model';
+
+export interface KysoCommentsUpdateEvent {
+  user: User;
+  organization: Organization;
+  team: Team;
+  comment: Comment;
+  discussion?: Discussion;
+  report?: Report;
+}
