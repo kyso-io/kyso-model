f8b115de1e22040efedbff231d755290c353c923
Fran Barrena <francisco@kyso.io>
2022-07-27 16:54:07 +0200

    Added Channel as alias of team in KysoConfigFile

--- src/models/kyso-config-file.model.ts
+++ src/models/kyso-config-file.model.ts
@@ -41,12 +41,21 @@ export class KysoConfigFile {
   @IsArray()
   public authors?: string[];
   
+  public get channel(): string {
+    return this.team;
+  }
+
+  public set channel(channel: string) {
+    this.team = channel;
+  }
+
   constructor(main: string, title: string, description: string, organization: string, team: string, tags: string[], type: ReportType | null) {
     this.main = main;
     this.title = title;
     this.description = description;
     this.organization = organization;
     this.team = team;
+    this.channel = team;
     this.tags = tags || [];
     this.type = type;
   }
