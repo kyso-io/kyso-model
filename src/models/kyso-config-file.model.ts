e240200f75a75e2d65c1fbcf639d0d1679686480
Fran Barrena <francisco@kyso.io>
2022-07-27 17:57:37 +0200

    simplified channel logic

--- src/models/kyso-config-file.model.ts
+++ src/models/kyso-config-file.model.ts
@@ -15,7 +15,7 @@ export class KysoConfigFile {
   public organization: string;
 
   @IsString()
-  private _team: string;
+  public team: string;
 
   @IsOptional()
   @IsString({ each: true })
@@ -40,36 +40,17 @@ export class KysoConfigFile {
   @IsOptional()
   @IsArray()
   public authors?: string[];
+
+  @IsOptional()
+  @IsString()
+  public channel?: string;
   
-  public get team(): string {
-    if(this._team) {
-      return this._team;
-    } else if(this.channel) {
-      return this.channel;
-    } else {
-      return "";
-    }
-  }
-
-  public set team(team: string) {
-    this._team = team;
-    this.channel = team;
-  }
-
-  public get channel(): string {
-    return this.team;
-  }
-
-  public set channel(channel: string) {
-    this.team = channel;
-  }
-
   constructor(main: string, title: string, description: string, organization: string, team: string, tags: string[], type: ReportType | null) {
     this.main = main;
     this.title = title;
     this.description = description;
     this.organization = organization;
-    this._team = team;
+    this.team = team;
     this.channel = team;
     this.tags = tags || [];
     this.type = type;
