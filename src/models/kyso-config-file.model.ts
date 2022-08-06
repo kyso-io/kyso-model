17e1c99b41c369a63cf096766cb1dda19cc036d7
Fran Barrena <francisco@kyso.io>
2022-08-06 17:28:35 +0200

    added parse methods for kysoConfigFile

--- src/models/kyso-config-file.model.ts
+++ src/models/kyso-config-file.model.ts
@@ -1,5 +1,6 @@
 import { IsArray, IsBoolean, IsOptional, IsString } from 'class-validator';
 import { ReportType } from '../enums/report-type.enum';
+import * as jsYaml from 'js-yaml'
 
 export class KysoConfigFile {
   @IsString()
@@ -54,6 +55,59 @@ export class KysoConfigFile {
     this.channel = team;
     this.tags = tags || [];
     this.type = type;
+  }
+
+  static fromObject(object: any): KysoConfigFile | null {
+    try {
+      const validationResult = KysoConfigFile.isValid(object);
+      if(validationResult.valid) {
+        const kysoFile: KysoConfigFile = new KysoConfigFile(
+          object.main, 
+          object.title,
+          object.description,
+          object.organization,
+          object.channel ? object.channel : object.team,
+          object.tags, 
+          object.type
+        );
+
+        // Now check and set the optional variables (less channel, which is already setted)
+        if(object.hasOwnProperty("hideRoot")) {
+          kysoFile.hideRoot = object.hideRoot;
+        }
+
+        if(object.hasOwnProperty("reports")) {
+          kysoFile.reports = object.reports;
+        }
+
+        if(object.hasOwnProperty("preview")) {
+          kysoFile.preview = object.preview;
+        }
+
+        if(object.hasOwnProperty("authors")) {
+          kysoFile.authors = object.authors;
+        }
+
+        return kysoFile;
+      } else {
+        console.log(`Provided yaml file has an invalid format: ${validationResult.message}`);
+        return null;
+      }
+    } catch(ex) {
+      console.log(`Unexpected error parsing kyso.yaml file`);
+      return null;
+    }
+  }
+
+  static fromYaml(yaml: string): KysoConfigFile | null {
+    const object: any = jsYaml.load(yaml);
+    return this.fromObject(object)
+  }
+
+  static fromJSON(json: string): KysoConfigFile | null {
+    const object: any = JSON.parse(json);
+
+    return this.fromObject(object)
   }
 
   static isValid(data: any): { valid: boolean; message: string } {
