99bab463447b92e71a73c1949477155aafc95f6e
Fran Barrena <francisco@kyso.io>
2022-08-08 17:47:23 +0200

    feat: added mentions and markedAsDeleted to InlineComment object

--- src/models/inline-comment.model.ts
+++ src/models/inline-comment.model.ts
@@ -6,13 +6,17 @@ export class InlineComment extends BaseModel {
   public user_id: string;
   public text: string;
   public edited: boolean;
+  public markedAsDeleted: boolean;
+  public mentions: string[];
 
-  constructor(report_id: string, cell_id: string, user_id: string, text: string) {
+  constructor(report_id: string, cell_id: string, user_id: string, text: string, edited: boolean, markedAsDelete: boolean, mentions: string[]) {
     super();
     this.report_id = report_id;
     this.cell_id = cell_id;
     this.user_id = user_id;
     this.text = text;
-    this.edited = false;
+    this.edited = edited;
+    this.markedAsDeleted = markedAsDelete;
+    this.mentions = mentions;
   }
 }
