460b8a08c43ca5a585669605f5f939567a6a55a1
Fran Barrena <francisco@kyso.io>
2022-08-08 17:50:53 +0200

    feat: added mentions to CreateInlineCommentDTO

--- src/dtos/create-inline-comment.dto.ts
+++ src/dtos/create-inline-comment.dto.ts
@@ -1,4 +1,4 @@
-import { IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+import { IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateInlineCommentDto {
   @IsMongoId()
@@ -11,4 +11,9 @@ export class CreateInlineCommentDto {
   @IsString()
   @IsNotEmpty()
   public text!: string;
+
+  @IsOptional()
+  @IsMongoId({ each: true })
+  public mentions!: string[];
+
 }
