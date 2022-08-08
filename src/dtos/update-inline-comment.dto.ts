0d513620731f1f7eaf0727e337fe6380796fcfe8
Fran Barrena <francisco@kyso.io>
2022-08-08 18:56:43 +0200

    added mentions to UpdateInlineCommentDto

--- src/dtos/update-inline-comment.dto.ts
+++ src/dtos/update-inline-comment.dto.ts
@@ -1,7 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateInlineCommentDto {
   @IsString()
   @IsNotEmpty()
   public text!: string;
+
+  @IsOptional()
+  @IsMongoId({ each: true })
+  public mentions!: string[];
 }
