abd178990b5ec90dc3cf4c1b95de8018722636b8
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-08-09 12:10:08 +0200

    feat: search user

---
+++ src/dtos/search-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class SearchUserDto {
+  @IsString()
+  @IsNotEmpty()
+  public organization_id!: string | null;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  public team_id!: string | null;
+
+  @IsString()
+  @IsNotEmpty()
+  public query!: string | null;
+}
