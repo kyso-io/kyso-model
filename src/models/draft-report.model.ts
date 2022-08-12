d81281814e45d0b2404f13447bf456245efa1960
Fran Barrena <francisco@kyso.io>
2022-08-12 17:50:20 +0200

    wrong examples

--- src/models/draft-report.model.ts
+++ src/models/draft-report.model.ts
@@ -49,46 +49,41 @@ export class DraftReport extends BaseModel {
 
   public static swaggerExamples(): any {
     return {
-      description: 'Examples',
-        required: true,
-        type: DraftReport,
-        examples: {
-            'Base object': {
-                summary: 'Base object',
-                value: new DraftReport(
-                          "title", 
-                          "description", 
-                          "creator_user_id", 
-                          "team_id", 
-                          "organization_id", 
-                          [], 
-                          "", 
-                          [], 
-                          []
-                        ),
-            },
-            'Example': {
-              summary: 'Example',
-              value: new DraftReport(
-                "My awesome report", 
-                "This is my description", 
-                "62f673e51315a21d21ac51f3", 
-                "62f673eeac3f21b5fac7cb99", 
-                "62f673f4d588100d9e2a1370", 
-                ["62f673fcb38f748212e9260d", "62f67403d88724f93da4b1a6"], 
-                "https://images.unsplash.com/photo-1590925888274-723778397410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGh1c2t5fGVufDB8fDB8fA%3D%3D&w=1000&q=80", 
-                [
-                  new CreateReportFileSystemItemDTO("62f67449e864e316fc3b5595", "index.md", "index.md", "md", 
-                  `## This is a draft
-
-                    * And this is
-                    * an example
-                  `)
-                ], 
-                []
-              ),
-          },
+        'Base object': {
+            summary: 'Base object',
+            value: new DraftReport(
+                      "title", 
+                      "description", 
+                      "creator_user_id", 
+                      "team_id", 
+                      "organization_id", 
+                      [], 
+                      "", 
+                      [], 
+                      []
+                    ),
         },
+        'Example': {
+          summary: 'Example',
+          value: new DraftReport(
+            "My awesome report", 
+            "This is my description", 
+            "62f673e51315a21d21ac51f3", 
+            "62f673eeac3f21b5fac7cb99", 
+            "62f673f4d588100d9e2a1370", 
+            ["62f673fcb38f748212e9260d", "62f67403d88724f93da4b1a6"], 
+            "https://images.unsplash.com/photo-1590925888274-723778397410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGh1c2t5fGVufDB8fDB8fA%3D%3D&w=1000&q=80", 
+            [
+              new CreateReportFileSystemItemDTO("62f67449e864e316fc3b5595", "index.md", "index.md", "md", 
+              `## This is a draft
+
+                * And this is
+                * an example
+              `)
+            ], 
+            []
+          )
+        }
     }
   }
 }
