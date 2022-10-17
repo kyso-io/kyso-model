4a2513cc9a656a2086b5b93d9607ba7ee6f07827
Daniel Andújar Lorenzo <dani@kyso.io>
2022-10-17 08:46:53 +0200

    feat: added team_visibility in ResourcePermissions

--- src/models/resource-permissions.model.ts
+++ src/models/resource-permissions.model.ts
@@ -1,4 +1,5 @@
 import { KysoPermissions } from '../enums/general-permissions.enum';
+import { TeamVisibilityEnum } from '../enums/team-visibility.enum';
 
 export class ResourcePermissions {
   public name: string;
@@ -15,7 +16,9 @@ export class ResourcePermissions {
 
   public role_names?: string[]
 
-  constructor(name: string, display_name: string, permissions: KysoPermissions[], id: string, organization_inherited: boolean, organization_id: string, role_names: string[]) {
+  public team_visibility?: TeamVisibilityEnum;
+
+  constructor(name: string, display_name: string, permissions: KysoPermissions[], id: string, organization_inherited: boolean, organization_id: string, role_names: string[], team_visibility: TeamVisibilityEnum) {
     this.name = name;
     this.display_name = display_name;
     this.permissions = permissions;
@@ -23,5 +26,6 @@ export class ResourcePermissions {
     this.organization_inherited = organization_inherited;
     this.organization_id = organization_id;
     this.role_names = role_names
+    this.team_visibility = team_visibility;
   }
 }
