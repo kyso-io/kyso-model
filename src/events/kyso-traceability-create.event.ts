126f4b8e84681de8fa146d79f8de9ea184265997
Daniel Andújar <dani@kyso.io>
2022-11-09 16:18:11 +0100

    feat: improvements in KysoTraceabilityCreateEvent

--- src/events/kyso-traceability-create.event.ts
+++ src/events/kyso-traceability-create.event.ts
@@ -1,12 +1,12 @@
 import { KysoEvent } from './kyso-event';
 
 export interface KysoTraceabilityCreateEvent extends KysoEvent {
-  user_id: string;
-  email: string;
+  user_id: string | null;
+  email: string | null;
   endpoint: string;
-  http_code_response: number;
+  http_response_code: number;
   user_agent: string;
   ip_address: string;
   request_method: string;
-  request_body: { [key: string]: any };
+  request_body: { [key: string]: any } | null;
 }
