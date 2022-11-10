19fcb694d5f0143b5aab527400e9781ed8babe1f
Fran Barrena <francisco@kyso.io>
2022-11-10 13:37:49 +0100

    fix: circular dependency between Hateoas and BaseModel

--- src/models/hateoas.model.ts
+++ src/models/hateoas.model.ts
@@ -2,13 +2,12 @@ import { ApiMethods } from '../interfaces/api-methods';
 import { StaticImplements } from '../types/static-implements';
 import { BaseModel } from './base.model';
 
-export class Hateoas extends BaseModel implements StaticImplements<ApiMethods<Hateoas>, typeof Hateoas> {
+export class Hateoas implements StaticImplements<ApiMethods<Hateoas>, typeof Hateoas> {
   public self_api: string;
 
   public self_ui: string;
 
   constructor(self_api: string, self_ui: string) {
-    super();
     this.self_api = self_api;
     this.self_ui = self_ui;
   }
