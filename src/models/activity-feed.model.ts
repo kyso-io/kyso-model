6f42f6ed393b3cb7ece4265cf32e8d09257b6964
Daniel Andújar <dani@kyso.io>
2022-11-11 10:09:52 +0100

    fix: added user_ids in ActivityFeed

--- src/models/activity-feed.model.ts
+++ src/models/activity-feed.model.ts
@@ -12,8 +12,18 @@ export class ActivityFeed extends BaseModel implements StaticImplements<ApiMetho
   public entity_id: string | null;
   public action: ActionEnum | null;
   public always_show: boolean;
+  public user_ids: string[];
 
-  constructor(user_id: string | null, organization: string | null, team: string | null, entity: EntityEnum | null, entity_id: string | null, action: ActionEnum | null, always_show: boolean) {
+  constructor(
+    user_id: string | null,
+    organization: string | null,
+    team: string | null,
+    entity: EntityEnum | null,
+    entity_id: string | null,
+    action: ActionEnum | null,
+    always_show: boolean,
+    user_ids: string[],
+  ) {
     super(null, null);
     this.user_id = user_id;
     this.organization = organization;
@@ -22,6 +32,7 @@ export class ActivityFeed extends BaseModel implements StaticImplements<ApiMetho
     this.entity_id = entity_id;
     this.action = action;
     this.always_show = always_show;
+    this.user_ids = user_ids;
   }
 
   validate(): boolean {
@@ -29,7 +40,7 @@ export class ActivityFeed extends BaseModel implements StaticImplements<ApiMetho
   }
 
   static createEmpty(): ActivityFeed {
-    return new ActivityFeed('', '', '', null, '', null, false);
+    return new ActivityFeed('', '', '', null, '', null, false, []);
   }
 
   static examples(): { [key: string]: { value: ActivityFeed } } {
