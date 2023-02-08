b9c5f50723d48942e83147c2566020665869eb8a
Fran Barrena <francisco@kyso.io>
2023-02-08 12:47:50 +0100

    feat: added silent property to signup

--- src/dtos/sign-up.dto.ts
+++ src/dtos/sign-up.dto.ts
@@ -1,4 +1,4 @@
-import { IsAlphanumeric, IsEmail, IsNotEmpty } from '@nestjs/class-validator';
+import { IsAlphanumeric, IsBoolean, IsEmail, IsNotEmpty, IsOptional } from '@nestjs/class-validator';
 import { ApiMethods } from '../interfaces/api-methods';
 import { StaticImplements } from '../types/static-implements';
 import { BaseDto } from './base.dto';
@@ -16,12 +16,25 @@ export class SignUpDto extends BaseDto implements StaticImplements<ApiMethods<Si
   @IsAlphanumeric()
   public password: string;
 
-  constructor(email: string, username: string, display_name: string, password: string) {
+  @IsOptional()
+  @IsBoolean()
+  /**
+   * If true, don't send any notification to the new created user. Useful for massive creation of users with the CLI
+   */
+  public silent: boolean;
+
+  constructor(email: string, username: string, display_name: string, password: string, silent?: boolean) {
     super();
     this.email = email;
     this.username = username;
     this.display_name = display_name;
     this.password = password;
+
+    if (silent) {
+      this.silent = silent;
+    } else {
+      this.silent = false;
+    }
   }
 
   validate(): boolean {
@@ -29,7 +42,7 @@ export class SignUpDto extends BaseDto implements StaticImplements<ApiMethods<Si
   }
 
   static createEmpty(): SignUpDto {
-    return new SignUpDto('', '', '', '');
+    return new SignUpDto('', '', '', '', false);
   }
 
   static examples(): { [key: string]: { value: SignUpDto } } {
