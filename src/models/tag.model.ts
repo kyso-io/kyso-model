5268ed1055925fa77086c79e26ce3b68437633a3
Daniel Andújar <daniandujar@gmail.com>
2023-03-17 09:50:24 +0100

    fix: tag belong to an org and team

--- src/models/tag.model.ts
+++ src/models/tag.model.ts
@@ -1,14 +1,22 @@
-import { IsNotEmpty } from '@nestjs/class-validator';
+import { IsMongoId, IsNotEmpty } from '@nestjs/class-validator';
 import { ApiMethods } from '../interfaces/api-methods';
 import { StaticImplements } from '../types/static-implements';
 import { BaseModel } from './base.model';
 
 export class Tag extends BaseModel implements StaticImplements<ApiMethods<Tag>, typeof Tag> {
+  @IsMongoId()
+  public organization_id: string;
+
+  @IsMongoId()
+  public team_id: string;
+
   @IsNotEmpty()
   public name: string;
 
-  constructor(name: string) {
+  constructor(organization_id: string, team_id: string, name: string) {
     super();
+    this.organization_id = organization_id;
+    this.team_id = team_id;
     this.name = name;
   }
 
@@ -17,7 +25,7 @@ export class Tag extends BaseModel implements StaticImplements<ApiMethods<Tag>, 
   }
 
   static createEmpty(): Tag {
-    return new Tag('');
+    return new Tag('', '', '');
   }
 
   static examples(): { [key: string]: { value: Tag } } {
